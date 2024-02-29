24fdd829ea5c350c3a09ffddcde701352385ce80
Neha Tiwari <nehaktiwaridny@gmail.com>
2024-02-29 18:39:52 +0530

    SQL COMMANDS PRACTICE

---
+++ DBMS Notes/student_details_202402291837.sql
@@ -0,0 +1,37 @@
+INSERT INTO org.student_details (stud_id,name,class,total_marks,gender,ADDRESS) VALUES
+	 (1,'Neha Tiwari','SIX',100,'Female','NG PARK'),
+	 (2,'John Deo','Four',75,'Female','NOT GIVEN YET'),
+	 (3,'Max Ruin','Three',85,'Male','NOT GIVEN YET'),
+	 (4,'Arnold','Three',55,'Male','NOT GIVEN YET'),
+	 (5,'John Mike','Four',60,'Female','NOT GIVEN YET'),
+	 (6,'Alex John','Four',55,'Male','NOT GIVEN YET'),
+	 (7,'My John Rob','Five',78,'Male','NOT GIVEN YET'),
+	 (8,'Asruid','Five',85,'Male','NOT GIVEN YET'),
+	 (9,'Tes Qry','Six',78,'Male','NOT GIVEN YET'),
+	 (10,'Big John','Four',55,'Female','NOT GIVEN YET');
+INSERT INTO org.student_details (stud_id,name,class,total_marks,gender,ADDRESS) VALUES
+	 (11,'Ronald','Six',89,'Female','NOT GIVEN YET'),
+	 (12,'Recky','Six',94,'Female','NOT GIVEN YET'),
+	 (13,'Kty','Seven',88,'Female','NOT GIVEN YET'),
+	 (14,'Bigy','Seven',88,'Female','NOT GIVEN YET'),
+	 (15,'Tade Row','Four',88,'Male','NOT GIVEN YET'),
+	 (16,'Gimmy','Four',88,'Male','NOT GIVEN YET'),
+	 (17,'Tumyu','Six',54,'Male','NOT GIVEN YET'),
+	 (18,'Honny','Five',75,'Male','NOT GIVEN YET'),
+	 (19,'Tinny','Nine',18,'Male','NOT GIVEN YET'),
+	 (20,'Jackly','Nine',65,'Female','NOT GIVEN YET');
+INSERT INTO org.student_details (stud_id,name,class,total_marks,gender,ADDRESS) VALUES
+	 (21,'Babby John','Four',69,'Female','NOT GIVEN YET'),
+	 (22,'Reggid','Seven',55,'Female','NOT GIVEN YET'),
+	 (23,'Herod','Eight',79,'Male','NOT GIVEN YET'),
+	 (24,'Tiddy Now','Seven',78,'Male','NOT GIVEN YET'),
+	 (25,'Giff Tow','Seven',88,'Male','NOT GIVEN YET'),
+	 (26,'Crelea','Seven',79,'Male','NOT GIVEN YET'),
+	 (27,'Big Nose','Three',81,'Female','NOT GIVEN YET'),
+	 (28,'Rojj Base','Seven',86,'Female','NOT GIVEN YET'),
+	 (29,'Tess Played','Seven',55,'Male','NOT GIVEN YET'),
+	 (30,'Reppy Red','Six',79,'Female','NOT GIVEN YET');
+INSERT INTO org.student_details (stud_id,name,class,total_marks,gender,ADDRESS) VALUES
+	 (31,'Marry Toeey','Four',88,'Male','NOT GIVEN YET'),
+	 (32,'Binn Rott','Seven',90,'Female','NOT GIVEN YET'),
+	 (33,'Kenn Rein','Six',96,'Female','NOT GIVEN YET');
